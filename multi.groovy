2bc664a53ccd63844804951732f6a91644f78155
Tejeshwar Raju R <az.teju@gmail.com>
2017-04-04 09:35:26 +0530

    Update multi.groovy

--- multi.groovy
+++ multi.groovy
@@ -1,7 +1,14 @@
-pipelineJob('example') {
-   scm {
-        git('https://github.com/Tejeshwarraju/com.mybank.online.git', 'master')
+multibranchPipelineJob('example') {
+    
+   branchSources {
+        git {
+            remote('https://github.com/Tejeshwarraju/com.mybank.online.git')
+        }
     }
+   orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+           
   definition {
         cps {
             script(readFileFromWorkspace('Jenkinsfile'))
