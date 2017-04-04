63bd624c0dd9be15b277cf134c6a090f896eed9c
Tejeshwar Raju R <az.teju@gmail.com>
2017-04-04 09:27:58 +0530

    Update multi.groovy

--- multi.groovy
+++ multi.groovy
@@ -2,7 +2,12 @@ pipelineJob('example') {
    scm {
         git('https://github.com/Tejeshwarraju/com.mybank.online.git', 'master')
     }
-  
+  definition {
+        cps {
+            script(readFileFromWorkspace('Jenkinsfile'))
+            sandbox()
+        }
+    }
     steps {
         steps {
         shell('python myscript.py')
